704ead0cbdf99d7581644238a988620600d0be30
jmshreyas882000-ux <jmshreyas882000@gmail.com>
2026-05-06 16:41:31 +0530

    Add User and Lead models with SQLAlchemy

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(200))
+
+class Lead(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
